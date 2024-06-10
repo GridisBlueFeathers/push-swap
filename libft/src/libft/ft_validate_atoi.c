b283c88e9f750c9654417999160f72968a248435
Serhii Veretennikov <gridis72@gmail.com>
2024-06-10 16:44:25 +0200

    fix: added itoa protection

--- libft/src/libft/ft_validate_atoi.c
+++ libft/src/libft/ft_validate_atoi.c
@@ -6,7 +6,7 @@
 /*   By: svereten <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 17:07:31 by svereten          #+#    #+#             */
-/*   Updated: 2024/06/10 15:45:33 by svereten         ###   ########.fr       */
+/*   Updated: 2024/06/10 16:44:07 by svereten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -18,7 +18,7 @@ int	ft_validate_atoi(char *str, int	*num)
 
 	str_num = ft_atoi(str);
 	str_itoa = ft_itoa(str_num);
-	if (ft_strncmp(str, str_itoa, ft_strlen(str)))
+	if (!str_itoa || ft_strncmp(str, str_itoa, ft_strlen(str)))
 		return (ft_free_n_null((void **)&str_itoa), 0);
 	*num = str_num;
 	return (ft_free_n_null((void **)&str_itoa), 1);
